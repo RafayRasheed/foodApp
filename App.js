fdd61f1c2dd69483f8581de8ff31785ecb9838f5
RafayRasheed <rafayrasheed777.rr@gmail.com>
2023-04-28 12:46:27 +0500

    28_04_2023_12_46

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StatusBar, Platform } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, SafeAreaView, StatusBar, Platform, TouchableOpacity, StyleSheet } from 'react-native';
+import { MMKV } from 'react-native-mmkv';
+import { myColors } from './ultils/myColors';
+
+const storage = new MMKV()
 
 export default function App() {
   console.log(`${Platform.OS} => Started Successfully`)
+  useEffect(() => {
+    storage.set("mberr", 'Hello')
+    console.log(storage.contains('mberr'))
+  }, [])
+  function getMMVK() {
+    const get = storage.getString('mberr')
+    console.log(get)
+  }
   return (
-    <SafeAreaView
-      style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-      <Text>Hello000000</Text>
-      <Text>Hello</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text>Hello000000</Text>
+        <Text>Hello</Text>
+      </View>
+      <TouchableOpacity activeOpacity={0.6} style={styles.containerButton} onPress={() => getMMVK()}>
+        <Text style={styles.textButton}>Button</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.background,
+  },
+
+  containerButton: {
+    backgroundColor: myColors.primary,
+    height: 40,
+    width: 140,
+    borderRadius: 10,
+    alignSelf: 'center',
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+
+  textButton: {
+    color: myColors.background
+  },
+})
---
+++ ultils/myColors.js
@@ -0,0 +1,28 @@
+export const myColors = {
+    primary: '#32B768',
+    textL: '#6B7280',
+    textL2: '#4B5563',
+    textL3: '#828282',
+    text: '#1F2937',
+    text2: '#242323',
+    background: '#FFFFFF',
+    backgroundL: '#F6F6F6',
+    backgroundTrans: 'rgba(0,0,0,0.5)',
+    blue: '#2C8DFF',
+    red: '#EB4646',
+    blue: '#2C8DFF',
+    statusbarW: '#FFFFFF',
+    statusbarG: '#32B768',
+    dot: '#E6E6E6',
+    lightGree: "#D1FAE5",
+    black: 'black',
+    black2: '#222222',
+    offColor: '#9CA3AF',
+    offColor2: "#89909E",
+    offColor3: '#F4F4F4',
+    offColor4: '#D2D4D8',
+    border: '#BEC5D1',
+    searchbar: '#E6E7E9'
+
+
+}
