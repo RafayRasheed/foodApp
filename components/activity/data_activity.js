c47a41afd174256f4cb2269e45d85056fa4847ff
RafayRasheed <rafayrasheed777.rr@gmail.com>
2023-05-10 17:07:16 +0500

    activityStart

---
+++ components/activity/data_activity.js
@@ -0,0 +1,117 @@
+export const history = [
+    {
+        name: 'McDonalds - King St',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $19.54',
+        status: 'Cancelled',
+        image: require('../assets/activity/item1.png')
+    },
+    {
+        name: 'Cabin in Barrie',
+        date: 'June 4 - June 9',
+        price: 'CAD $1,900.34',
+        status: 'Completed',
+        image: require('../assets/activity/item2.png')
+    },
+    {
+        name: 'KFC - Lawrence Ave',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $27.74',
+        status: 'Completed',
+        image: require('../assets/activity/item3.png')
+    },
+    {
+        name: '100 King St ',
+        date: 'June 10 - 2:39PM',
+        price: 'CAD $19.54',
+        status: 'Completed',
+        image: require('../assets/activity/item4.png')
+    },
+    {
+        name: 'McDonalds - King St',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $19.54',
+        status: 'Cancelled',
+        image: require('../assets/activity/item1.png')
+    },
+    {
+        name: 'Cabin in Barrie',
+        date: 'June 4 - June 9',
+        price: 'CAD $1,900.34',
+        status: 'Completed',
+        image: require('../assets/activity/item2.png')
+    },
+    {
+        name: 'KFC - Lawrence Ave',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $27.74',
+        status: 'Completed',
+        image: require('../assets/activity/item3.png')
+    },
+    {
+        name: '100 King St ',
+        date: 'June 10 - 2:39PM',
+        price: 'CAD $19.54',
+        status: 'Completed',
+        image: require('../assets/activity/item4.png')
+    },
+]
+
+export const Order = [
+    {
+        name: 'McDonalds - King St',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $19.54',
+        status: 'Cancelled',
+        image: require('../assets/activity/item1.png')
+    },
+    {
+        name: 'Cabin in Barrie',
+        date: 'June 4 - June 9',
+        price: 'CAD $1,900.34',
+        status: 'Completed',
+        image: require('../assets/activity/item2.png')
+    },
+    {
+        name: 'KFC - Lawrence Ave',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $27.74',
+        status: 'Completed',
+        image: require('../assets/activity/item3.png')
+    },
+    {
+        name: '100 King St ',
+        date: 'June 10 - 2:39PM',
+        price: 'CAD $19.54',
+        status: 'Completed',
+        image: require('../assets/activity/item4.png')
+    },
+    {
+        name: 'McDonalds - King St',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $19.54',
+        status: 'Cancelled',
+        image: require('../assets/activity/item1.png')
+    },
+    {
+        name: 'Cabin in Barrie',
+        date: 'June 4 - June 9',
+        price: 'CAD $1,900.34',
+        status: 'Completed',
+        image: require('../assets/activity/item2.png')
+    },
+    {
+        name: 'KFC - Lawrence Ave',
+        date: 'June 4 - 12:39PM',
+        price: 'CAD $27.74',
+        status: 'Completed',
+        image: require('../assets/activity/item3.png')
+    },
+    {
+        name: '100 King St ',
+        date: 'June 10 - 2:39PM',
+        price: 'CAD $19.54',
+        status: 'Completed',
+        image: require('../assets/activity/item4.png')
+    },
+]
