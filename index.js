9a8fdbe4bd5d7c2930092eb2eeea876247e0de59
Rafay Rasheed <rafayrasheed777.rr@gmail.com>
2023-09-03 16:33:12 +0500

    fyt

--- index.js
+++ index.js
@@ -2,20 +2,34 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { Alert, AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import { navigateNoti, navigationRef } from './components/RootNavigation';
-import * as RootNavigation from './components/RootNavigation';
+import { navigateNoti, navigationRef, notificationListeners } from './components/RootNavigation';
+import NavigationService from './components/NavigationService';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
+    // setTimeout(() => {
+    //     NavigationService.navigate("OrderDetails", {})
+    // }
+    //     , 1000)
+    // messaging().onNotificationOpenedApp(async remoteMessage => {
+    //     console.log(
+    //         ' bACK Notification caused app to open from background state:',
+    //         remoteMessage,
+    //     );
+
+    messaging().se
+    // });
 });
 messaging().getInitialNotification(async remoteMessage => {
     // navigateNoti('OrderDetails', {})
-    RootNavigation.navigate("OrderDetails", {});
 
-    console.log('Message handled in the kill!', remoteMessage);
+
+    console.log('Message handled in the kill!', remoteMessage)
+
+
 });
+
 AppRegistry.registerComponent(appName, () => App);
